257cf9ca5526d6f4b2343b4b96efbb7d35c3f7e4
isaacNuflo <isaac.nuflo21@gmail.com>
2018-06-26 13:00:31 -0500

    Update questionCreate.e6b323e759b4.js

--- staticfiles/js/questionCreate.e6b323e759b4.js
+++ staticfiles/js/questionCreate.e6b323e759b4.js
@@ -41,8 +41,8 @@ $(document).ready(function () {
         alternativa5 = $("#Alternativa5").val();
         informacion = $("#info").val();
 
-        var obj = new crearObj(subtema, pregunta, alternativa1, alternativa2, alternativa3,
-            alternativa3, alternativa4, alternativa5, informacion, dificultad, correcta);
+        var obj = new crearObj(subtema, pregunta, alternativa1, alternativa2, alternativa3, 
+                               alternativa4, alternativa5, informacion, dificultad, correcta);
 
         enviarData(obj);
         $("#Alternativa1").val("");
