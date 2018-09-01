5ffd8e7736d63912de8e1c184b140b44b1f3d64f
Isaac Nuflo <isaac.nuflo21@gmail.com>
2018-08-31 22:54:24 -0500

    usuario_tema

--- servicios/serializers.py
+++ servicios/serializers.py
@@ -3,39 +3,35 @@ from servicios.models import *
 
 
 class AsignaturaSerializer(ModelSerializer):
-
     class Meta:
         model = Asignatura
         fields = '__all__'
 
 
 class DificultadSerializer(ModelSerializer):
-
     class Meta:
         model = Dificultad
         fields = '__all__'
 
 
 class TemaAsignaturaSerializer(ModelSerializer):
-    
     class Meta:
         model = Tema
         fields = '__all__'
 
 
 class SubtemaTemaSerializer(ModelSerializer):
-    
     class Meta:
         model = Subtema
-        fields = '__all__'        
+        fields = '__all__'
 
 
 class TemaSerializer(ModelSerializer):
     asignatura = SerializerMethodField()
 
     def get_asignatura(self, tema):
-      asignatura = AsignaturaSerializer(tema.id_asignatura,many=False,read_only=True)
-      return asignatura.data
+        asignatura = AsignaturaSerializer(tema.id_asignatura, many=False, read_only=True)
+        return asignatura.data
 
     class Meta:
         model = Tema
@@ -46,7 +42,7 @@ class SubtemaSerializer(ModelSerializer):
     tema = SerializerMethodField()
 
     def get_tema(self, subtema):
-        tema = TemaSerializer(subtema.id_tema,many=False,read_only=True)
+        tema = TemaSerializer(subtema.id_tema, many=False, read_only=True)
         return tema.data
 
     class Meta:
@@ -55,14 +51,12 @@ class SubtemaSerializer(ModelSerializer):
 
 
 class TipoPreguntaSerializer(ModelSerializer):
-
     class Meta:
         model = TipoPregunta
         fields = '__all__'
 
 
 class PreguntaTSerializer(ModelSerializer):
-    
     class Meta:
         model = Pregunta
         fields = '__all__'
@@ -72,26 +66,25 @@ class PreguntaSerializer(ModelSerializer):
     dificultad = SerializerMethodField()
     subtema = SerializerMethodField()
     tipo_pregunta = SerializerMethodField()
-    
-    def get_tipo_pregunta(self,pregunta):
-        tipo_pregunta = TipoPreguntaSerializer(pregunta.id_tipopregunta,many=False,read_only=True)
+
+    def get_tipo_pregunta(self, pregunta):
+        tipo_pregunta = TipoPreguntaSerializer(pregunta.id_tipopregunta, many=False, read_only=True)
         return tipo_pregunta.data
 
-    def get_subtema(self,pregunta):
-        subtema = SubtemaSerializer(pregunta.id_subtema,many=False,read_only=True)
+    def get_subtema(self, pregunta):
+        subtema = SubtemaSerializer(pregunta.id_subtema, many=False, read_only=True)
         return subtema.data
 
-    def get_dificultad(self,pregunta):
-        dificultad = DificultadSerializer(pregunta.id_dificultad,many=False, read_only=True)
+    def get_dificultad(self, pregunta):
+        dificultad = DificultadSerializer(pregunta.id_dificultad, many=False, read_only=True)
         return dificultad.data
- 
+
     class Meta:
         model = Pregunta
         fields = '__all__'
 
 
 class RespuestaSerializer(ModelSerializer):
-
     class Meta:
         model = Respuesta
         fields = '__all__'
@@ -100,7 +93,7 @@ class RespuestaSerializer(ModelSerializer):
 class RespuestaPreguntaSerializer(ModelSerializer):
     pregunta = SerializerMethodField()
 
-    def get_pregunta(self,respuesta):
+    def get_pregunta(self, respuesta):
         pregunta = PreguntaSerializer(respuesta.id_pregunta, many=False, read_only=True)
         return pregunta.data
 
@@ -110,14 +103,12 @@ class RespuestaPreguntaSerializer(ModelSerializer):
 
 
 class UsuarioSerializer(ModelSerializer):
-
     class Meta:
         model = Usuario
         fields = '__all__'
 
 
 class UsuarioAuthSerializer(ModelSerializer):
-
     class Meta:
         model = Usuario
         fields = ('usuario', 'password')
@@ -126,7 +117,7 @@ class UsuarioAuthSerializer(ModelSerializer):
 class UsuarioAsignaturaSerializer(ModelSerializer):
     asignatura = SerializerMethodField()
 
-    def get_asignatura(self,usuario_asignatura):
+    def get_asignatura(self, usuario_asignatura):
         asignatura = AsignaturaSerializer(usuario_asignatura.id_asignatura, many=False, read_only=True)
         return asignatura.data
 
@@ -136,6 +127,11 @@ class UsuarioAsignaturaSerializer(ModelSerializer):
 
 
 class UsuarioTemaSerializer(ModelSerializer):
+    tema = SerializerMethodField()
+
+    def get_tema(self, usuario_tema):
+        tema = TemaSerializer(usuario_tema.id_tema, many=False, read_only=True)
+        return tema.data
 
     class Meta:
         model = UsuarioTema
@@ -143,21 +139,18 @@ class UsuarioTemaSerializer(ModelSerializer):
 
 
 class UsuarioSubtemaSerializer(ModelSerializer):
-
     class Meta:
         model = UsuarioSubtema
         fields = '__all__'
 
 
 class UsuarioArticuloSerializer(ModelSerializer):
-
     class Meta:
         model = UsuarioArticulo
         fields = '__all__'
 
 
 class ArticuloSerializer(ModelSerializer):
-
     class Meta:
         model = Articulo
         fields = '__all__'
